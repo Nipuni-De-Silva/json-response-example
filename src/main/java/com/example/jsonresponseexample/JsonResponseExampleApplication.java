c6c59057e95c60d17140b4e59d6841d83e67bc09
Nipuni De Silva <nipunisha274@gmail.com>
2023-12-24 19:14:50 +0530

    Initial commit

---
+++ src/main/java/com/example/jsonresponseexample/JsonResponseExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonresponseexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonResponseExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonResponseExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jsonresponseexample/JsonResponseExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jsonresponseexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonResponseExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
